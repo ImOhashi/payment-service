408130838f3c48a5d8454a557d5eec2797457166
ImOhashi <leonardoohashi.mkt@gmail.com>
2022-11-27 23:18:09 -0300

    feat: set kafka template with producer config

--- src/main/java/com/ohashi/paymentservice/service/impl/PaymentServiceImpl.java
+++ src/main/java/com/ohashi/paymentservice/service/impl/PaymentServiceImpl.java
@@ -2,15 +2,23 @@ package com.ohashi.paymentservice.service.impl;
 
 import com.ohashi.paymentservice.model.Payment;
 import com.ohashi.paymentservice.service.PaymentService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
+
+import java.io.Serializable;
 
 @Log4j2
 @Service
+@RequiredArgsConstructor
 public class PaymentServiceImpl implements PaymentService {
+
+    private final KafkaTemplate<String, Serializable> kafkaTemplate;
 
     @Override
     public void sendPayment(Payment payment) {
         log.info("Received payment: {}", payment);
+        kafkaTemplate.send("payment-topic", payment);
     }
 }
